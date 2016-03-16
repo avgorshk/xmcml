bf2b3b0c6f3e924713bd5f51031be6fb0b5915cc
umkavmk <cr.for.mo@gmail.com>
2016-03-16 20:28:50 +0300

    Update all programs. Added calculations and web-site xmcml.

---
+++ branches/gerasimov/xmcml + Depth Map/xmcml/mcml_integration.h
@@ -0,0 +1,8 @@
+#ifndef __MCML_INTEGRATION_H
+#define __MCML_INTEGRATION_H
+
+#include "mcml_types.h"
+
+double ComputeWeightIntegral(double dot, double anisotropy, double eps, uint n);
+
+#endif //__MCML_INTEGRATION_H
